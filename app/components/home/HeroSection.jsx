f10397e87f5e1acd7f06a87e9e40f79a55557603
Meeshbhoombah <rohanmishra2017@gmail.com>
2026-09-22 03:09:32 -0400

    Improved Hero Section copy

--- app/components/home/HeroSection.jsx
+++ app/components/home/HeroSection.jsx
@@ -2,13 +2,13 @@ export default function HeroSection() {
   return (
     <section className="home-section hero-section">
       <p>
-        <strong>Work with Rohan "Meeshbhoombah" Mishra. Build perfect decentralized systems.</strong>
+        <strong>Work with Rohan. Build perfect decentralized systems.</strong>
       </p>
       <p>
         I build community, design and install resilient coordination mechanisms, 
-        improve governance, solidfy decision-making, and achieve outcomes. Built
-        off 13 years of Full-Stack engineering, distributed systems, and 
-        Rust-focued since 2019. 
+        improve governance, solidfy decision-making, and achieve outcomes. 
+        13 years of Full-Stack engineering, distributed systems, and 
+        Rust-focused since 2019. 
       </p>
     </section>
   );
