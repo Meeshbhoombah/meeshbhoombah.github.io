c36416ee7f10ecc9b53412afdf4167b8287d0955
Meeshbhoombah <rohanmishra2017@gmail.com>
2026-09-26 11:28:54 -0400

    Updated hero content

--- app/components/home/HeroSection.jsx
+++ app/components/home/HeroSection.jsx
@@ -2,14 +2,41 @@ export default function HeroSection() {
   return (
     <section className="home-section hero-section">
       <p>
-        <strong>Work with Rohan. Build perfect decentralized systems.</strong>
+        <strong>
+        Governance-focused decentralized systems engineering.
+        </strong>
       </p>
       <p>
-        I build community, design and install resilient coordination mechanisms, 
-        improve governance, solidfy decision-making, and achieve outcomes. 
-        13 years of Full-Stack engineering, distributed systems, and 
-        Rust-focused since 2019. 
+        Refine your organization's composite and affecting mechanisms of 
+        community, customer base, internal processes, software, and teams.
+        Created $11.5B. About people. 
       </p>
     </section>
   );
 }
+
+/*
+        My communities, internal stakeholders, and customers experience 
+        customized or best-in-class mechanisms installed with precision for 
+        growth and scale. I have helped 8+ organizations 5x revenue drivers on 
+        average, accrue a cumiliative sum of valuations nearing $48M, and led 
+        teams sizing 2 to 12.
+
+*/
+/*
+        13+ years in technology; specialized in community & governance.
+        Decentralized systems engineering, hacking, and startup operating. 
+*/
+
+/*
+ *      Decentralized systems engineering, hacking, and generalist 
+        startup operator with 13 years of experience working on systems. 
+        Design reslient coordination mechanisms
+*/
+
+/*
+        Rohan "Meesh" Mishra builds communities, designs resilient coordination 
+        mechanisms, improves governance, solidifies decision-making, and 
+        achieves outcomes. 13 years of Full-Stack engineering, distributed and 
+        decentralized systems, and Rust-focused since 2019. 
+*/
